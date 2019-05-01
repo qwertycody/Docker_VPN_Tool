9ff00ab62ad7ab319226446ce22aa64f397be4d4
Cody Garrett <cody@garrett.ms>
2019-05-01 15:27:14 -0400

    Added GUI menu to allow for recreation of tunnels instead of complete restart - also utilities added

--- run_website.sh
+++ run_website.sh
@@ -8,13 +8,12 @@
 
 declare -A DESTINATION_LIST
 DESTINATION_LIST+=(["2021"]="google.com:80")
-DESTINATION_LIST+=(["2022"]="google.com:80")
 
 ##################################
 ### VPN Endpoint to Connect To ###
 ##################################
 
-OPENCONNECT_VPN_ENDPOINT="vpn.company.com"
+OPENCONNECT_VPN_ENDPOINT="vpn.companyname.com"
 
 ########################################
 ### Local Running Instance of Docker ###
@@ -27,38 +26,65 @@ OPENCONNECT_VPN_ENDPOINT="vpn.company.com"
 LOCAL_DOCKER_ADDRESS="192.168.99.100"
 
 #Desired Port for SSH Server in Docker - Used for Tunnel
-LOCAL_DOCKER_SSH_PORT="2020"
+LOCAL_DOCKER_SSH_PORT="3030"
 
 #Docker Parameters for Setup, Creation, and Identification
-CONTAINER_NAME="vpn_company"
+CONTAINER_NAME="vpn_abc"
 IMAGE_NAME="garrett_tech:vpn_tool"
 
-##################################
-### VPN Endpoint to Connect To ###
-##################################
+function connectToVpn()
+{
+    ##################################
+    ### VPN Endpoint to Connect To ###
+    ##################################
 
-sh dockerVpnTool.sh $OPENCONNECT_VPN_ENDPOINT $LOCAL_DOCKER_SSH_PORT $CONTAINER_NAME $IMAGE_NAME
+    sh dockerVpnTool.sh $OPENCONNECT_VPN_ENDPOINT $LOCAL_DOCKER_SSH_PORT $CONTAINER_NAME $IMAGE_NAME
+}
 
-for LOCAL_LISTEN_PORT in "${!DESTINATION_LIST[@]}"
-do
-    #Pull Values from Array
-    DESTINATION_ADDRESS_DESTINATION_PORT=${DESTINATION_LIST[$LOCAL_LISTEN_PORT]}
+function establishTunnels()
+{
+    clear
 
-    echo "Establishing Tunnel to $DESTINATION_ADDRESS_DESTINATION_PORT using Local Port $LOCAL_LISTEN_PORT"
+    for LOCAL_LISTEN_PORT in "${!DESTINATION_LIST[@]}"
+    do
+        #Pull Values from Array
+        DESTINATION_ADDRESS_DESTINATION_PORT=${DESTINATION_LIST[$LOCAL_LISTEN_PORT]}
 
-    #Linux
-    ssh-keygen -R [$LOCAL_DOCKER_ADDRESS]:$LOCAL_DOCKER_SSH_PORT
-    ssh -o StrictHostKeyChecking=no -fNL $LOCAL_LISTEN_PORT:$DESTINATION_ADDRESS_DESTINATION_PORT -l root $LOCAL_DOCKER_ADDRESS -p $LOCAL_DOCKER_SSH_PORT
+        echo "Establishing Tunnel to $DESTINATION_ADDRESS_DESTINATION_PORT using Local Port $LOCAL_LISTEN_PORT"
 
-    #Windows - Comment Linux Lines Above 
-    #nohup echo y | "/c/Program Files/PuTTY/plink.exe" -N -L $LOCAL_LISTEN_PORT:$DESTINATION_ADDRESS_DESTINATION_PORT -ssh root@$LOCAL_DOCKER_ADDRESS -P $LOCAL_DOCKER_SSH_PORT -pw password >/dev/null 2>&1 &
+        ssh-keygen -R [$LOCAL_DOCKER_ADDRESS]:$LOCAL_DOCKER_SSH_PORT
+        ssh -o StrictHostKeyChecking=no -fNL $LOCAL_LISTEN_PORT:$DESTINATION_ADDRESS_DESTINATION_PORT -l root $LOCAL_DOCKER_ADDRESS -p $LOCAL_DOCKER_SSH_PORT >/dev/null 2>&1
 
-    echo ""
-    echo "##################################################################"
-    echo "If there are no errors up to this point you can now connect"
-    echo "to the endpoint using localhost:$LOCAL_LISTEN_PORT using ssh/rdp"
-    echo "##################################################################"
-    echo ""
-done
+        #nohup echo y | "/c/Program Files/PuTTY/plink.exe" -N -L $LOCAL_LISTEN_PORT:$DESTINATION_ADDRESS_DESTINATION_PORT -ssh root@$LOCAL_DOCKER_ADDRESS -P $LOCAL_DOCKER_SSH_PORT -pw password >/dev/null 2>&1 &
 
-read
+        echo ""
+        echo "##################################################################"
+        echo "If there are no errors up to this point you can now connect"
+        echo "to the endpoint using localhost:$LOCAL_LISTEN_PORT using ssh/rdp"
+        echo "##################################################################"
+        echo ""
+    done
+}
+
+choiceFunction() {
+    SUCCESS="false"
+ 
+    while [ $SUCCESS == "false" ]
+    do
+        clear
+        echo "Please enter your choice - $CONTAINER_NAME: "
+        echo "1 - Connect and Establish Tunnels"
+        echo "2 - Establish Tunnels"
+        echo "3 - Quit"
+        read CHOICE
+ 
+        case $CHOICE in
+           "1") SUCCESS="true";connectToVpn; establishTunnels;;
+           "2") SUCCESS="true";establishTunnels;;
+           "3") SUCCESS="true";echo "Exiting ..."; exit;;
+           *) read -p "Invalid choice, Press any key to continue..." fakeVariableIgnoreMe;;
+        esac
+    done
+}
+
+choiceFunction
---
+++ util_stopAll.sh
@@ -0,0 +1,9 @@
+docker kill $(docker ps -q)
+
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    taskill //f //im ssh.exe
+    taskill //f //im plink.exe
+else
+    pkill -f ssh
+    pkill -f plink
+fi
