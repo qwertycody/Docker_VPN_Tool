a120321471adde923e8b3f86a5c4f0dd436ff00f
Cody Garrett <cody@garrett.ms>
2019-05-02 09:28:21 -0400

    Removed need for screen command and a+d key combo

--- dockerVpnTool.sh
+++ dockerVpnTool.sh
@@ -10,19 +10,8 @@ PORT_SSH="$LOCAL_DOCKER_SSH_PORT:22"
 
 docker run -d --privileged --rm --name $CONTAINER_NAME -p $PORT_SSH $IMAGE_NAME
 
-clear 
-
-echo "##################################################################"
-echo "THE NEXT PROMPT WILL ASK YOU TO ENTER A FEW THINGS"
-echo "ONCE YOU ARE FULLY CONNECTED TO THE VPN ENDPOINT"
-echo "PRESS CTRL+A+D TO DETACH AND CONTINUE THE CONNECTION PROCESS"
-echo "IF YOU UNDERSTAND THESE INSTRUCTIONS PRESS ENTER TO CONTINUE"
-echo "##################################################################"
-
-read 
-
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    winpty docker exec -it $CONTAINER_NAME bash -c "screen openconnect $OPENCONNECT_VPN_ENDPOINT"
+    winpty docker exec -it $CONTAINER_NAME bash -c "openconnect -b $OPENCONNECT_VPN_ENDPOINT"
 else 
-    docker exec -it $CONTAINER_NAME bash -c "screen openconnect $OPENCONNECT_VPN_ENDPOINT"
+    docker exec -it $CONTAINER_NAME bash -c  "openconnect -b $OPENCONNECT_VPN_ENDPOINT"
 fi
